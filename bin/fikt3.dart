abb6dab4f9ac027cc472067114561ceba1daad6f
EliasKIngvarsson <iljakarevski@gmail.com>
2022-01-03 18:33:13 +0000

    fikt3komið inn

---
+++ bin/fikt3.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:convert';
+
+
+// Write a program that accepts a number from user and counts the number of digits in the given integer using loop
+//
+
+void main(List<String>argumnet){
+  for (int i= 0; i < 100; i++){
+
+    print(i);
+    print('lol');
+  }
+
+
+}
